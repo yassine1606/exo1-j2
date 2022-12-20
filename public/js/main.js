8ab6d8a978899e6ca44b1e70f37f3b437f099095
Yassine Ebd <elbodmoussiyassine@gmail.com>
2022-12-20 09:44:01 +0100

    fin

---
+++ public/js/main.js
@@ -0,0 +1,25 @@
+let nombre = 8.56
+
+console.log(Math.round(nombre));
+
+console.log(Math.ceil(nombre));
+
+console.log(Math.floor(nombre));
+
+console.log(Math.trunc(nombre));
+
+console.log(Math.random());
+
+console.log(Math.random()*100);
+
+console.log(Math.trunc(Math.random()*100));
+
+console.log(Math.pow(2, 8));
+
+console.log(Math.sqrt(9));
+
+console.log(Math.abs(-1));
+
+console.log(Math.max(-2,8,1000,57));
+
+console.log(Math.min(-2,8,1000,57));
